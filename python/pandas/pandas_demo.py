d4e8d0771cbaed70077f415c96f38fb43a0e9a3f
archieli <279848421@qq.com>
2022-02-01 23:09:52 +0800

    group.apply应用，*args和**kwargs使用

--- python/pandas/pandas_demo.py
+++ python/pandas/pandas_demo.py
@@ -14,7 +14,7 @@ class PandasDemo():
         gender = ["男", "女"]
         smoker = [True, False]
         education = ["本科", "硕士", "博士"]
-        dict_data = {
+        dict_random_data = {   #随机生成的数据
             "height":np.random.randint(160, 190, ROW_NUM),   #身高数据，从160到190，构造ROW_NUM个
             "weight":np.random.randint(60, 80, ROW_NUM),
             "gender":[gender[x] for x in np.random.randint(0, 2, ROW_NUM)],
@@ -22,7 +22,15 @@ class PandasDemo():
             "education":[education[x] for x in np.random.randint(0, 3, ROW_NUM)]
         }
 
-        self.demo_df = pd.DataFrame(data=dict_data)
+        dict_fixed_data = {  #固定的数据，便于观测结果
+            "height":[160,  170,    170,    170,    172,    180,    170,    172,    180,    181],
+            "weight":[60,   65,     70,     66,     65,     70,     80,     70,     73,     75],
+            "gender":['男', '女',   '女',   '女',   '男',   '女',   '男',   '女',   '男',   '女'],
+            "smoker":[True, False,  False,  True,   True,   False,  True,   False,  False,  False],
+            "education":["本科", "本科", "硕士", "硕士", "博士", "博士", "博士", "硕士", "本科", "本科"],
+        }
+
+        self.demo_df = pd.DataFrame(data=dict_fixed_data)
     
 
     def dataframe_groupby_map(self):
@@ -36,10 +44,32 @@ class PandasDemo():
         print(self.demo_df.groupby('gender').aggregate(np.sum))
 
         
+    def apply_sum(self, group_df:pd.DataFrame, *args, **kwargs):
+        """每个group的dataframe作为输入，计算这一组group的结果"""
+        
+        sum = 0
+        for item in args:
+            sum = sum + item
+
+        sum = sum + kwargs['base1']
+        sum = sum + kwargs['base2']
+        
+        for row in group_df.itertuples():
+            sum = sum + row[1]
+
+        return sum, group_df.shape[0], group_df.shape[1]
+
+    
+    def dataframe_groupby_apply_test(self):
+        """测试dataframe的groupby的apply操作。"""
+
+        print(self.demo_df.groupby('education').apply(self.apply_sum, 1, 2, 3, base1=1, base2=2))
+
 
 if __name__=='__main__':
 
     demo = PandasDemo()
-    demo.dataframe_groupby_map()
+    #demo.dataframe_groupby_map()
     #demo.dataframe_groupby_test()
+    demo.dataframe_groupby_apply_test()
 
